bffcd4e8bc397d18d43551a45b08c1838f416472
Song-of-your-soul <yauznik007@gmail.com>
2024-04-22 22:31:43 +0300

    Update Homework_9.py

--- Homework_9.py
+++ Homework_9.py
@@ -12,16 +12,16 @@ def cats_in_hats(cats: int, rounds: int) -> list:
     round = 1
     cats_with_hats = []
     while round <= rounds:
-        checked_cats = pick_cat(round, cats)
-        for cat in range(cats + 1)[1:]:
-            if cat in checked_cats:
-                if cat in cats_with_hats:
-                    cats_with_hats.remove(cat)
-                else:
-                    cats_with_hats.append(cat)
-        checked_cats.clear()
+        checked_cats = pick_cat(cats, round)
+        for cat in checked_cats:
+            if cat in cats_with_hats:
+                cats_with_hats.remove(cat)
+            else:
+                cats_with_hats.append(cat)
         round += 1
     return cats_with_hats
 
+
+print(cats_in_hats(100, 100))
 # Поки що не дуже певен, що вмію рахувати складність операцій,
-# але якщо правильно розумію, то тут вона O(n)
+# але якщо правильно розумію, то тут вона O(m*n)
